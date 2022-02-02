8fcc944ee2b52f09fef65394919989ac163a5148
heusyfelype <felype.heusy@gmail.com>
2022-02-02 15:30:48 -0300

    Carta adicionada ao jogo

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
-function comecarOJogo(){
+function comecarOJogo() {
     document.querySelector("button").classList.add("escondido")
 
     let quantidadeDeCartas = prompt("Digite quantas cartas você quer no jogo (entre 4 e 14 cartas, número par)")
 
-    while( quantidadeDeCartas%2 != 0 || quantidadeDeCartas < 4 || quantidadeDeCartas > 14){
+    while (quantidadeDeCartas % 2 != 0 || quantidadeDeCartas < 4 || quantidadeDeCartas > 14) {
         quantidadeDeCartas = prompt("Digite quantas cartas válido (entre 4 e 14 cartas, número par)")
     }
 
+    document.querySelector("section").innerHTML = ` 
+    <div class="card">
+    <div class="front-face face">
+      Frente
+    </div>
+    <div class="back-face face">
+      Verso
+    </div>
+  </div>
     
+    `
+
+
+
 }
