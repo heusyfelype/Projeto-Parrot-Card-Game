d82e2beb548d0a63874acedf323d634b2ef61ffa
heusyfelype <felype.heusy@gmail.com>
2022-02-02 15:05:44 -0300

    Adicionada a verificação da quantidade de cartas e se é numero par

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 function comecarOJogo(){
     document.querySelector("button").classList.add("escondido")
+
+    let quantidadeDeCartas = prompt("Digite quantas cartas você quer no jogo (entre 4 e 14 cartas, número par)")
+
+    while( quantidadeDeCartas%2 != 0 || quantidadeDeCartas < 4 || quantidadeDeCartas > 14){
+        quantidadeDeCartas = prompt("Digite quantas cartas válido (entre 4 e 14 cartas, número par)")
+    }
+
+    
 }
