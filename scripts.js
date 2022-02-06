044c6c91f0d103c3254753470e5d8b0d8424700f
heusyfelype <felype.heusy@gmail.com>
2022-02-05 22:21:54 -0300

    Adicionado as imagens ao verso do card, correção de bugs e adicionado os bonus

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
+let quantidadeDeCartas = 0;
+
 function comecarOJogo() {
-    document.querySelector("button").classList.add("escondido")
+    document.querySelector("section").innerHTML = "";
+    if (!document.querySelector("button").classList.contains("escondido")) {
+        document.querySelector("button").classList.add("escondido")
+    }
 
-    let quantidadeDeCartas = parseInt(prompt("Digite quantas cartas você quer no jogo (entre 4 e 14 cartas, número par)"))
 
+    quantidadeDeCartas = parseInt(prompt("Digite quantas cartas você quer no jogo (entre 4 e 14 cartas, número par)"))
+    
     while (quantidadeDeCartas % 2 != 0 || quantidadeDeCartas < 4 || quantidadeDeCartas > 14) {
         quantidadeDeCartas = prompt("Digite quantas cartas válido (entre 4 e 14 cartas, número par)")
     }
 
     // o maximo de cartas diferente que eu posso ter 
-    let bancoDeImagensDiferentes = ["verso1", "verso2", "verso3", "verso4", "verso5", "verso6", "verso7"]
+    let bancoDeImagensDiferentes = ["<img src='imagens-e-gifs/bobrossparrot.gif'>", "<img src='imagens-e-gifs/explodyparrot.gif'>", "<img src='imagens-e-gifs/fiestaparrot.gif'", "<img src='imagens-e-gifs/metalparrot.gif'", "<img src='imagens-e-gifs/revertitparrot.gif'", "<img src='imagens-e-gifs/tripletsparrot.gif'", "<img src='imagens-e-gifs/unicornparrot.gif'"]
     //Vamos embaralhar essas cartas
     let bancoDeImagensDiferentesEmbaralhado = bancoDeImagensDiferentes.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
     // preciso pegar apenas a quantidade de cartas DIFERENTES necessárias para o jogo i.e. quantidadeDeCartas/2
@@ -52,13 +58,16 @@ function comecarOJogo() {
 let contador = 0;
 let primeiraCartaVirada = null;
 let segundaCartaVirada = null;
+let quantidadeDeCartasViradas = 0;
+let quantidadeDeParesAcertados = 0;
 function clicarNaCarta(elementoClicado, clique) {
+    quantidadeDeCartasViradas += 1;
     contador = contador + clique;
 
     if (contador == 1) {
         if (elementoClicado.querySelector(".front-face").classList.contains("rotacionar_frente") &&
             elementoClicado.querySelector(".back-face").classList.contains("rotacionar_verso")) {
-            
+
 
 
         } else {
@@ -73,20 +82,40 @@ function clicarNaCarta(elementoClicado, clique) {
     if (contador == 2) {
         if (elementoClicado == primeiraCartaVirada) {
             contador = 1;
-            alert("Voce clicou na mesma carta");
+            //alert("Voce clicou na mesma carta");
         } else {
             elementoClicado.querySelector(".front-face").classList.add("rotacionar_frente");
             elementoClicado.querySelector(".back-face").classList.add("rotacionar_verso");
             segundaCartaVirada = elementoClicado;
             if (primeiraCartaVirada.querySelector(".back-face").innerHTML == segundaCartaVirada.querySelector(".back-face").innerHTML) {
-                //alert("as cartas se manterão viradas");
+                primeiraCartaVirada.removeAttribute("onclick")
+                segundaCartaVirada.removeAttribute("onclick")
+                quantidadeDeParesAcertados += 1;
+
+                if (quantidadeDeParesAcertados == quantidadeDeCartas / 2) {
+                    alert("Você ganhou em " + quantidadeDeCartasViradas + " jogadas!")
+                    let reiniciar = prompt("Deseja reiniciar o jogo? \"sim\" ou \"não\"");
+                    if (reiniciar = "sim") {
+                        quantidadeDeCartas = 0;
+                        contador = 0;
+                        primeiraCartaVirada = null;
+                        segundaCartaVirada = null;
+                        quantidadeDeCartasViradas = 0;
+                        quantidadeDeParesAcertados = 0;
+
+                        clearInterval(intervalo);
+                        segundos = 0;
+                        comecarOJogo();
+                        cronometrar();
+                    }
+                }
             }
 
         }
     }
 
-    console.log(contador);
-    console.log(primeiraCartaVirada, segundaCartaVirada)
+    //console.log(contador);
+    //console.log(primeiraCartaVirada, segundaCartaVirada)
 
     if (contador == 3 && segundaCartaVirada != null && primeiraCartaVirada.querySelector(".back-face").innerHTML == segundaCartaVirada.querySelector(".back-face").innerHTML) {
         elementoClicado.querySelector(".front-face").classList.add("rotacionar_frente");
@@ -113,59 +142,27 @@ function clicarNaCarta(elementoClicado, clique) {
 
     }
 
-    else if(contador == 3 || contador == 2  && segundaCartaVirada == null){
+    else if (contador == 3 || contador == 2 && segundaCartaVirada == null) {
         contador = 1;
     }
 
+}
 
+let segundos = parseInt(document.querySelector("article p").innerHTML);
+let intervalo = null
 
+function cronometrar() {
+    document.querySelector("article").classList.remove("escondido")
 
+    intervalo = setInterval(iniciarOCronometro, 1000)
+}
 
-
-
-
-
-
-
-    // if (elementoClicado != elementoClicadoSalvo) {
-    //     contador = contador + clique;
-    //     console.log(contador, elementoClicado, elementoClicadoSalvo)
-
-    //     if (contador % 2 != 0) { //eh impar
-    //         elementoClicado.querySelector(".front-face").classList.add("rotacionar_frente")
-    //         elementoClicado.querySelector(".back-face").classList.add("rotacionar_verso")
-
-
-    //     } else { //eh par
-    //         elementoClicado.querySelector(".front-face").classList.add("rotacionar_frente")
-    //         elementoClicado.querySelector(".back-face").classList.add("rotacionar_verso")
-
-    //         // console.log(elementoClicado.querySelector(".back-face").innerHTML)
-    //         // console.log(elementoClicadoSalvo.querySelector(".back-face").innerHTML)
-
-    //         // if (elementoClicado.querySelector(".back-face").innerHTML != elementoClicadoSalvo.querySelector(".back-face").innerHTML) {
-    //         //     elementoClicado.querySelector(".front-face").classList.add("rotacionar_frente")
-    //         //     elementoClicado.querySelector(".back-face").classList.add("rotacionar_verso")
-
-    //         //     // elementoClicado.querySelector(".front-face").classList.remove("rotacionar_frente")
-    //         //     // elementoClicado.querySelector(".back-face").classList.remove("rotacionar_verso")
-
-    //         //     // elementoClicadoSalvo.querySelector(".front-face").classList.remove("rotacionar_frente")
-    //         //     // elementoClicadoSalvo.querySelector(".back-face").classList.remove("rotacionar_verso")
-    //         // }
-    //         // else if (elementoClicado.querySelector(".back-face").innerHTML == elementoClicadoSalvo.querySelector(".back-face").innerHTML) {
-    //         //     elementoClicado.querySelector(".front-face").classList.add("rotacionar_frente")
-    //         //     elementoClicado.querySelector(".back-face").classList.add("rotacionar_verso")
-
-    //         // }
-
-    //     }
-    //     elementoClicadoSalvo = elementoClicado;
-    // }
-    // else {
-    //     elementoClicado.querySelector(".front-face").classList.toggle("rotacionar_frente")
-    //     elementoClicado.querySelector(".back-face").classList.toggle("rotacionar_verso")
-    // }
-
-
+function iniciarOCronometro() {
+    if (quantidadeDeParesAcertados == quantidadeDeCartas / 2) {
+        clearInterval(intervalo);
+        //     //alert("Hora da janta!");
+    } else {
+        segundos += 1;
+        document.querySelector("article p").innerHTML = segundos;
+    }
 }
